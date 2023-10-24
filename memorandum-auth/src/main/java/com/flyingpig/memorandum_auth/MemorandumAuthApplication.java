f48240ec1189d01245000b6c804221f22c9f1395
flying_pig <1839976096@qq.com>
2023-10-24 23:19:16 +0800

    搭建完基础架构

--- memorandum-auth/src/main/java/com/flyingpig/memorandum_auth/MemorandumAuthApplication.java
+++ memorandum-auth/src/main/java/com/flyingpig/memorandum_auth/MemorandumAuthApplication.java
@@ -2,6 +2,7 @@ package com.flyingpig.memorandum_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MemorandumAuthApplication {
@@ -9,5 +10,4 @@ public class MemorandumAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemorandumAuthApplication.class, args);
     }
-
 }
